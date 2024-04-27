29dfe1db36849193bd58498ba05e23a5b05bb7ea
Aufa Billah <aufaroot18@gmail.com>
2024-04-27 21:43:35 +0700

    feat: bootstrap final project (#1)
    * feat: bootstrap app
    
    * feat: setup structure
    
    * feat: setup unit test

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div>
+      <h2>Covid App</h2>
+      <h3>Final Project Frontend Engineer</h3>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
