a230f575c2aa7a6171ed81f70a087e1243c79325
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-05-02 09:22:18 +0700

    chore: customize tailwind themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+
+    extend: {
+      screens: {
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors: {
+        'primary': '#06D6A0',
+        'secondary': '#118AB2',
+        'tertiary': '#d3dce6',
+        'danger': '#EF476F',
+        'accent': '#073B4C',
+      },
+      fontFamily: {
+        display: ['Poppins'],
+        sub: ['Merriweather', 'Oswald'],
+      },
+    },
   },
   plugins: [],
 }
