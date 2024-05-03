dc57dec194fee006d7b80ec744d524cd3c396f18
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-05-04 01:43:41 +0700

    refactor: redefine variable & obj property

---
+++ src/utils/constants/data.js
@@ -0,0 +1,39 @@
+const data = {
+    menu: [
+        {
+            name: "Home",
+            id: "#home",
+        },
+        {
+            name: "Statistic",
+            id: "#statistic",
+        },
+        {
+            name: "Province",
+            id: "#province",
+        },
+        {
+            name: "Form",
+            id: "#form",
+        },
+    ],
+    statis: [
+        {
+            title: "Confirmed",
+            value: "605.646",
+            variant: "text-primary",
+        },
+        {
+            title: "Recoverd",
+            value: "90.000",
+            variant: "text-secondary",
+        },
+        {
+            title: "Dead",
+            value: "5000",
+            variant: "text-danger",
+        },
+    ],
+};
+
+export default data;
