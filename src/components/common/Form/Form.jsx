5bf1cd2d58de7a1c44d6330a2268d8d6f1d0203b
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-07-06 15:15:14 +0700

    feat: improvment form handle & add input validation errors

--- src/components/common/Form/Form.jsx
+++ src/components/common/Form/Form.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
 import { ProvinceContext } from "../../../context/ProvinceContext";
 import {
  FormContainer,
@@ -13,96 +14,101 @@ import Input from "../../ui/Input";
 import Select from "../../ui/Select";
 import Button from "../../ui/Button";
 
-const Form = () => {
+const Form = React.forwardRef(({}, ref) => {
  const { provinceData, handleAddData } = useContext(ProvinceContext);
- const [formData, setFormData] = React.useState({
-  name: "",
-  status: "",
-  amount: 0,
- });
+ const {
+  register,
+  handleSubmit,
+  formState: { errors },
+  reset,
+ } = useForm();
 
- const { name, status, amount } = formData;
-
- const handleChange = (e) => {
-  const { name, value } = e.target;
-  setFormData((prevData) => ({
-   ...prevData,
-   [name]: value,
-  }));
- };
-
- const handleSubmit = (e) => {
-  e.preventDefault();
-  handleAddData(name, status, parseInt(amount));
-
+ const onSubmit = (data) => {
+  handleAddData(data.name, data.status, parseInt(data.amount));
   window.scrollTo({
    top: 600,
    behavior: "smooth",
   });
-
-  setFormData({
-   name: "",
-   status: "",
-   amount: 0,
+  reset({
+   name: "a",
+   status: "a",
+   amount: "",
   });
  };
 
  const inputStyles = {
   width: "100%",
-  "@media (min-width: 768px)": {
+  "@media (minWidth: 768px)": {
    width: "50%",
   },
  };
 
  return (
-  <FormContainer>
+  <FormContainer ref={ref}>
    <FormContent>
     <FormLeft>
      <SvgForm />
     </FormLeft>
     <FormRight>
      <FormTitle>Add Data</FormTitle>
-     <form onSubmit={handleSubmit}>
+     <form onSubmit={handleSubmit(onSubmit)}>
       <FormRow>
        <Input style={inputStyles}>
         <Input.Label label="Province" htmlFor="name" />
-        <Select id="name" name="name" value={name} onChange={handleChange}>
-         <Select.Option selected disabled value="">
+        <Select
+         style={{ color: "gray" }}
+         {...register("name", { required: true })}
+         onFocus={(e) => (e.target.style.color = "black")}
+        >
+         <option selected disabled value="a">
           Choose Province
-         </Select.Option>
+         </option>
          {provinceData.map((item, index) => (
           <Select.Option key={index} value={item.name} name={item.name} />
          ))}
         </Select>
+        {errors.name && (
+         <span style={{ color: "red", fontSize: "12px" }}>
+          Province is required
+         </span>
+        )}
        </Input>
        <Input style={inputStyles}>
         <Input.Label label="Status" htmlFor="status" />
         <Select
-         id="status"
-         name="status"
-         value={status}
-         onChange={handleChange}
+         style={{ color: "gray" }}
+         {...register("status", { required: true })}
+         onFocus={(e) => (e.target.style.color = "black")}
         >
-         <Select.Option selected disabled value="">
+         <option selected disabled value="a">
           Choose Status
-         </Select.Option>
+         </option>
          <Select.Option value="confirmed" name="Confirmed" />
          <Select.Option value="recovered" name="Recovered" />
          <Select.Option value="treatment" name="Treatment" />
          <Select.Option value="death" name="Death" />
         </Select>
+        {errors.status && (
+         <span style={{ color: "red", fontSize: "12px" }}>
+          Status is required
+         </span>
+        )}
        </Input>
       </FormRow>
       <div style={{ marginTop: "20px" }}>
        <Input>
         <Input.Label label="Amount" htmlFor="amount" />
         <Input.Field
+         placeholder="Add total amount of cases"
          type="number"
          id="amount"
-         name="amount"
-         value={amount}
-         onChange={handleChange}
+         {...register("amount", { required: true, min: 1 })}
         />
+        {errors.amount && (
+         <span style={{ color: "red", fontSize: "12px" }}>
+          Amount is required and must be greater than 0
+         </span>
+        )}
        </Input>
       </div>
       <Button
@@ -118,6 +124,6 @@ const Form = () => {
    </FormContent>
   </FormContainer>
  );
-};
+});
 
 export default Form;
